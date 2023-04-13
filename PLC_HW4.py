19af831e00135eba89249609f8d564aaaac6ff59
Ibrahima Bah <73560688+ibrahimabah-hub@users.noreply.github.com>
2023-04-12 23:22:09 -0400

    PLC HW4
    HW assignment of creating a recursive descent algorithm that parses through tokens and returns true if the list of tokens is in the language.

---
+++ PLC_HW4.py
@@ -0,0 +1,166 @@
+
+  
+def check(same, tokens):
+  global curToken
+  if(curToken==same):
+    if(len(tokens)>1):
+      tokens=tokens[1:]
+      curToken=tokens[0]
+      return True
+    else:
+      curToken=None
+      return True
+
+def stmtList(tokens):
+  if(curToken==None):
+    return False
+  stmt(tokens)
+  while(curToken==';'):
+    check(';', tokens)
+    stmt(tokens)
+    
+def tokenize(tokens):
+  global curToken
+  curToken = newList[0]
+  stmtList(tokens)
+  return curToken==None
+  
+def stmt(tokens):
+  if(curToken==None):
+    return False
+  elif(curToken=='if'):
+    ifStmt(tokens)
+  elif(curToken=='while'):
+    whileLoop(tokens)
+  elif(curToken=='{'):
+    block(tokens)
+  else:
+    expr(tokens)
+def whileLoop(tokens):
+  if(curToken==None):
+    return False
+  else:
+    check('while', tokens)
+    check('(', tokens)
+    boolExpr(tokens)
+    check(')', tokens)
+    if(curToken=='{'):
+      block(tokens)
+    else:
+      stmt(tokens)
+      check(';', tokens)
+
+def ifStmt(tokens):
+  if(curToken==None):
+    return False
+  else:
+    check('if', tokens)
+    check('(', tokens)
+    boolExpr(tokens)
+    check(')', tokens)
+    if(curToken=='{'):
+      block(tokens)
+    else:
+      stmt(tokens)
+      check(';', tokens)
+    if(curToken=='else'):
+      check('else')
+      if(curToken=='{'):
+        block(tokens)
+      else:
+        stmt(tokens)
+        check(';', tokens)
+
+def block(tokens):
+  if(curToken==None):
+    return False
+  check('{', tokens)
+  stmtList(tokens)
+  check('}', tokens)
+
+def expr(tokens):
+  if(curToken==None):
+    return False
+  term(tokens)
+  while(curToken=='+' or curToken=='-'):
+    if(curToken=='+'):
+      check('+', tokens)
+    else:
+      check('-', tokens)
+    term(tokens)
+
+def term(tokens):
+  if(curToken==None):
+    return False
+  fact(tokens)
+  while(curToken=='*' or curToken=='/' or curToken=='%'):
+    if(curToken=='*'):
+      check('*', tokens)
+    elif(curToken=='%'):
+      check('%', tokens)
+    else:
+      check('/', tokens)
+    fact(tokens)
+
+def fact(tokens):
+  global curToken
+  if(curToken==None):
+    return False
+  if(curToken=='('):
+    check('(')
+    expr(tokens)
+    check(')')
+  else:
+    if(len(tokens)>1):
+      tokens=tokens[1:]
+      curToken=tokens[0]
+    else:
+      curToken=None
+
+def boolExpr(tokens):
+  if(curToken==None):
+    return False
+  bterm(tokens)
+  while(curToken=='>' or curToken=='<' or curToken=='>=' or curToken=='<='):
+    if(curToken=='>'):
+      check('>', tokens)
+    elif(curToken=='<'):
+      check('<', tokens)
+    elif(curToken=='>='):
+      check('>=', tokens)
+    else:
+      check('<=', tokens)
+    bterm(tokens)
+
+def bterm(tokens):
+  if(curToken==None):
+    return False
+  band(tokens)
+  while(curToken=='==' or curToken=='!='):
+    if(curToken=='=='):
+      check('==', tokens)
+    else:
+      check('!=', tokens)
+    band(tokens)
+
+def band(tokens):
+  if(curToken==None):
+    return False
+  bor(tokens)
+  while(curToken=='&&'):
+    check('&&', tokens)
+    bor(tokens)
+
+def bor(tokens):
+  if(curToken==None):
+    return False
+  expr(tokens)
+  while(curToken=='&&'):
+    check('&&', tokens)
+    expr(tokens)
+
+newList=['Hello', ';']
+if(tokenize(newList)):
+  print('correct')
+else:
+  print('incorrect')
